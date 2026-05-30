80c616f304db26607b80336bf2a60436c8544030
Reddikhaien <fredflug@online.no>
2026-05-30 11:04:00 +0200

    base url and static

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin()],
+  static: true,
+  base: './',
   server: {
     port: 3000,
   },
